2fbcabb88a26f9e9279766861cbcf97baea7080c
Mike Setzer <michaelsetzerucf@gmail.com>
2023-03-09 14:20:03 -0500

    Template placeholders swapped

--- functions.php
+++ functions.php
@@ -1,11 +1,10 @@
 <?php
-namespace MyProject\Theme;
 
-define( 'MYPROJECT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'UCFCARD_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once MYPROJECT_THEME_DIR . 'includes/config.php';
-include_once MYPROJECT_THEME_DIR . 'includes/meta.php';
+include_once UCFCARD_THEME_DIR . 'includes/config.php';
+include_once UCFCARD_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,10 @@
 /**
  * Handle all theme configuration here
  **/
-namespace MyProject\Theme\Includes\Config;
 
 
-define( 'MYPROJECT_THEME_URL', get_stylesheet_directory_uri() );
-define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
-define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
-define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
-define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+define( 'UCFCARD_THEME_URL', get_stylesheet_directory_uri() );
+define( 'UCFCARD_THEME_STATIC_URL', UCFCARD_THEME_URL . '/static' );
+define( 'UCFCARD_THEME_CSS_URL', UCFCARD_THEME_STATIC_URL . '/css' );
+define( 'UCFCARD_THEME_JS_URL', UCFCARD_THEME_STATIC_URL . '/js' );
+define( 'UCFCARD_THEME_IMG_URL', UCFCARD_THEME_STATIC_URL . '/img' );

1ab8411143383e0ad65642aad38ca15e91b67608
Mike Setzer <michaelsetzerucf@gmail.com>
2023-04-20 16:06:40 -0400

    Added ACF fields and jquery collapse to departments page

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,14 @@
 
 // Require your theme's custom script files here
 // ...
+jQuery(document).ready(($) => {
+  $('#staffToggle, #badgeToggle').on('click', function (event) {
+    event.preventDefault(); // Prevent default behavior of the anchor tag
+
+    const targetForm = $(this).attr('id') === 'staffToggle' ? '#staffForm' : '#badgeForm';
+    const otherForm = $(this).attr('id') === 'staffToggle' ? '#badgeForm' : '#staffForm';
+
+    $(targetForm).collapse('toggle');
+    $(otherForm).collapse('hide');
+  });
+});
--- template-departments.php
+++ template-departments.php
@@ -7,7 +7,69 @@
 <?php get_header(); the_post(); ?>
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
-	<?php the_content(); ?>
+	<?php
+	//Fetch Department ACF Fields
+	$staff_image = get_field('staff_faculty_id_image');
+	$staff_overlay = get_field('staff_faculty_overlay_text');
+	$badge_image = get_field('badge_image');
+	$badge_overlay = get_field('badge_overlay_text');
+	$staff_content = get_field('staff_faculty_content');
+	$badge_content = get_field('badge_content');
+	?>
+
+	<div class="container">
+		<div class="row">
+			<?php if( $staff_image ): ?>
+				<div class="mb-4 col-md-6 col-lg-4" id="staffToggle">
+
+					<a class="media-background-container gtm-section-links-cards d-block w-100 h-100 px-4 pt-5 pb-4 hover-parent text-secondary hover-text-inverse text-decoration-none" href="#staffForm" data-toggle="collapse" aria-expanded="false" aria-controls="collapseExample">
+
+						<img src="<?php echo esc_url( $staff_image['url'] ); ?>" alt="<?php echo esc_attr( $staff_image['alt'] ); ?>" class=" media-background object-fit-cover hover-child hover-child-scale-up" data-object-fit="cover">
+
+						<div class="media-background object-fit-cover bg-inverse-t-3 hover-child hover-child-show fade" data-object-fit="cover"></div>
+
+						<h3 class="h5 text-uppercase mt-5 mb-3"><?php echo $staff_overlay; ?></h3>
+
+						<span class="fa fa-2x fa-chevron-down hover-child hover-child-show fade mt-5 mb-3" aria-hidden="true"></span>
+
+					</a>
+
+				</div>
+			<?php endif; ?>
+
+			<?php if( $badge_image ): ?>
+				<div class="mb-4 col-md-6 col-lg-4" id="badgeToggle">
+
+					<a class="media-background-container gtm-section-links-cards d-block w-100 h-100 px-4 pt-5 pb-4 hover-parent text-secondary hover-text-inverse text-decoration-none" href="#staffForm" data-toggle="collapse" aria-expanded="false" aria-controls="collapseExample">
+
+						<img src="<?php echo esc_url( $badge_image['url'] ); ?>" alt="<?php echo esc_attr( $badge_image['alt'] ); ?>" class=" media-background object-fit-cover hover-child hover-child-scale-up" data-object-fit="cover">
+
+						<div class="media-background object-fit-cover bg-inverse-t-3 hover-child hover-child-show fade" data-object-fit="cover"></div>
+
+						<h3 class="h5 text-uppercase mt-5 mb-3"><?php echo $badge_overlay; ?></h3>
+
+						<span class="fa fa-2x fa-chevron-down hover-child hover-child-show fade mt-5 mb-3" aria-hidden="true"></span>
+
+					</a>
+
+				</div>
+			<?php endif; ?>
+		</div>
+		<div class="row mt-4">
+			<div class="col-md-12">
+				<div class="collapse" id="staffForm">
+					<form>
+						<?php echo $staff_content; ?>
+					</form>
+				</div>
+				<div class="collapse" id="badgeForm">
+					<form>
+						<?php echo $badge_content; ?>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </article>
 
 <?php get_footer(); ?>

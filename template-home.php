b0d8be6a21111ec094225b968f8b35e328268be4
Mike Setzer <michaelsetzerucf@gmail.com>
2023-04-24 14:02:19 -0400

    Menu functions, home template

---
+++ template-home.php
@@ -0,0 +1,58 @@
+<div class="jumbotron jumbotron-fluid py-5 bg-inverse-t-2 mb-0">
+	<div class="container text-center py-lg-4 px-lg-5">
+		<h2 class="display-4 font-condensed mb-5">Welcome to Card Services</h2>
+		<hr class="hr-3 hr-primary my-4" style="width: 15%;" />
+		<div class="row py-2">
+			<div class="col-12">
+				<p class="mb-5 lead">Welcome to Card Services, your one-stop shop for all things related to your UCF Card. Here you will learn where you can add money to your Knight Cash Account, where to use your card on campus, and other features that will enhance your student experience.</p>
+				<a class="btn btn-lg btn-primary" href="https://ucf.qualtrics.com/jfe/form/SV_5znjgd5nI1ppqmy">Request Card by Mail</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid bg-faded">
+	<p><!------- Our Services Row --------></p>
+
+	<div class="row">
+		<div class="col-12 col-md-4 text-md-center bg-primary-t-3 p-4 p-md-5">
+			<div class="row mb-3">
+				<div class="col-12 d-flex align-items-center justify-content-center"><span class="font-weight-black text-uppercase h3 letter-spacing-1 d-block mb-0 pt-1 pt-md-0">Forms</span></div>
+			</div>
+			<hr class="hr-3 hr-white w-25 my-3 my-md-4" />
+			<p class="mb-3">For requesting approved department endorsed ID cards and badges.</p>
+			<a class="btn btn-secondary" href="/departments/">Learn More</a>
+		</div>
+
+		<div class="col-12 col-md-4 text-md-center bg-faded p-4 p-md-5">
+			<div class="row mb-3">
+				<div class="col-12 d-flex align-items-center justify-content-center mb-0">
+					<img src="https://ucfcardredesignbscmsdev.smca.ucf.edu/wp-content/uploads/sites/40/2023/04/knight-cash-icon.png" alt="Knight Cash Logo" class="inline-block pr-3" style="max-height:50px;">
+					<span class="font-weight-black text-uppercase h3 letter-spacing-1 d-block mb-0 pt-1 pt-md-0">Knight Cash</span></div>
+			</div>
+			<hr class="hr-3 hr-primary w-25 my-3 my-md-4" />
+			<p class="mb-3">Knight Cash is one of the most convenient payment methods to use at UCF. There are three convenient and secure ways to add Knight Cash to your UCF Card (Online, at the Card Office, and Loader Kiosks). Knight Cash can be used to pay for everything on campus from laundry and vending machines to food vendors and Health Center co-pays.</p>
+			<a class="btn btn-primary" href="/knight-cash/">Learn More</a>
+		</div>
+
+		<div class="col-12 col-md-4 text-md-center bg-primary-t-3 p-4 p-md-5">
+			<div class="row mb-3">
+				<div class="col-12 d-flex align-items-center justify-content-center"><span class="font-weight-black text-uppercase h3 letter-spacing-1 d-block mb-0 pt-1 pt-md-0">UCF Connect &amp; Online Students</span></div>
+			</div>
+			<hr class="hr-3 hr-white w-25 my-3 my-md-4" />
+			<p class="mb-3">You may obtain your UCF Card at Card Services, UCF Connect Centers, or by mail.</p>
+			<a class="btn btn-secondary" href="/distance-learning/">Learn More</a>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<p><!------- Updates Row --------></p>
+	<div class="row py-5">
+		<div class="col-12">
+			<h2 class="heading-underline mb-5 text-secondary">Updates</h2>
+		</div>
+		<div class="col-12">
+			[ucf-post-list layout="news" category="homepage"]
+		</div>
+	</div>
+</div>

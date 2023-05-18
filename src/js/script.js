71f60be7b1ebba49e95cef809e13b156f96fafd3
Mike Setzer <michaelsetzerucf@gmail.com>
2023-05-18 10:55:16 -0400

    Department form js

--- src/js/script.js
+++ src/js/script.js
@@ -15,14 +15,22 @@ jQuery(document).ready(($) => {
   $('#staffToggle, #badgeToggle').on('click', function (event) {
     event.preventDefault(); // Prevent default behavior of the anchor tag
 
+    // Set the target form and the collapsing form
     const targetForm = $(this).attr('id') === 'staffToggle' ? '#staffForm' : '#badgeForm';
     const otherForm = $(this).attr('id') === 'staffToggle' ? '#badgeForm' : '#staffForm';
 
+    // Show the correct form, hide the others
     if ($(targetForm).hasClass('show')) {
       $(targetForm).collapse('hide');
     } else {
       $(targetForm).collapse('show');
       $(otherForm).collapse('hide');
     }
+  });
+
+  $('#staffForm, #badgeForm').on('shown.bs.collapse', function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 500);
   });
 });
